9e6293ab610bd330f78be2d5803e4c386ff7ace8
Rex Wang <rwang@central1.com>
2016-02-11 16:00:58 -0800

    working on it

--- app/app.js
+++ app/app.js
@@ -7,22 +7,36 @@
 
   App.Router.map(function() {
     this.route('home', { path: '/' });
+    this.route('devices');
   });
 
-  // App.Device = DS.Model.extend({
-  //  identifier: DS.attr('string'),
-  //  lastUpdated: DS.attr('string'),
-  //  userName: DS.attr('string'),
-  //  serialNumber: DS.attr('string'),
-  //  deviceName: DS.attr('string'),
-  //  make: DS.attr('string'),
-  //  model: DS.attr('string'),
-  //  os: DS.attr('string')
-  // });
+  App.Device = DS.Model.extend({
+   identifier: DS.attr('string'),
+   lastUpdated: DS.attr('string'),
+   userName: DS.attr('string'),
+   serialNumber: DS.attr('string'),
+   deviceName: DS.attr('string'),
+   make: DS.attr('string'),
+   model: DS.attr('string'),
+   os: DS.attr('string')
+  });
 
   App.HomeRoute = Ember.Route.extend({
     model: function() {
       return $.getJSON('public/MOCK_DATA.json');
+    }
+  });
+
+  App.HomeController = Ember.Controller.extend({
+    actions: {
+      add: function() {
+
+      },
+      remove: function() {
+        this.get('model').shift();
+
+        this.set('model', this.get('model'));
+      }
     }
   });
 })();
